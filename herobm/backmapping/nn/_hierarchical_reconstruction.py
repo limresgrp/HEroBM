eb423b1fd019f9b6b56b47bb6382c8e1c83161c6
Daniangio <daniele.angioletti0@gmail.com>
2025-05-14 15:09:49 +0200

    Fixed _hierarchical_reconstruction and invariant loss to handle batches > 1 + added loss on dihedrals on invariant loss

--- herobm/backmapping/nn/_hierarchical_reconstruction.py
+++ herobm/backmapping/nn/_hierarchical_reconstruction.py
@@ -67,60 +67,62 @@ class HierarchicalReconstructionModule(GraphModuleMixin, torch.nn.Module):
         if torch.any(torch.isnan(bead2atom_relative_vectors)):
             raise Exception("NaN")
         bead_pos = data[AtomicDataDict.POSITIONS_KEY]
+        bead_pos_slices        = data.get("pos_slices")
         atom_pos_slices        = data.get("atom_pos_slices")
         idcs_mask              = data.get("bead2atom_reconstructed_idcs")
         idcs_mask_slices       = data.get("bead2atom_reconstructed_idcs_slices", torch.tensor([0, len(bead_pos)], dtype=int, device=bead_pos.device))
         weights                = data.get("bead2atom_reconstructed_weights")
         level_idcs_mask        = data.get("lvl_idcs_mask")
         level_idcs_anchor_mask = data.get("lvl_idcs_anchor_mask")
-        center_atoms = torch.unique(data[AtomicDataDict.EDGE_INDEX_KEY][0])
+        bead_types             = data.get(AtomicDataDict.NODE_TYPE_KEY).squeeze(-1)
+        center_atoms           = torch.unique(data[AtomicDataDict.EDGE_INDEX_KEY][0])
+        n_reconstructed_beads  = len(center_atoms)
+
+        # Initialize predicted atom_pos
+        reconstructed_atom_pos = torch.empty(n_reconstructed_beads, atom_pos_slices[-1], 3, dtype=torch.float32, device=bead2atom_relative_vectors.device)
+        reconstructed_atom_pos[:] = torch.nan
 
         if self.normalize_b2a_rel_vec:
-            # with torch.no_grad():
             # norm_factor = bead2atom_relative_vectors.max().item()
-            # bead2atom_relative_vectors /= norm_factor
             norm = torch.norm(bead2atom_relative_vectors, dim=-1, keepdim=True)
             bead2atom_relative_vectors = bead2atom_relative_vectors / (norm + 1.e-5)
-            bead2atom_relative_vectors = bead2atom_relative_vectors * self.atom_type2bond_lengths[data.get(AtomicDataDict.NODE_TYPE_KEY).squeeze(-1)]
+            bead2atom_relative_vectors = bead2atom_relative_vectors * self.atom_type2bond_lengths[bead_types]
 
-        for (b2a_idcs_from, b2a_idcs_to), atom_pos_from in zip(
-            zip(idcs_mask_slices[:-1], idcs_mask_slices[1:]),
-            atom_pos_slices[:-1],
+        for b2a_idcs_from, b2a_idcs_to, bead_pos_from, atom_pos_from in zip(
+            idcs_mask_slices[:-1], idcs_mask_slices[1:],
+            bead_pos_slices[:-1],  atom_pos_slices[:-1],
         ):
             batch_center_atoms = center_atoms[(center_atoms>=b2a_idcs_from) & (center_atoms<b2a_idcs_to)]
 
             b2a_idcs = idcs_mask[batch_center_atoms]
             batch_weights = weights[batch_center_atoms]
-            reconstructed_atom_pos = torch.empty(len(batch_center_atoms), atom_pos_slices[-1], 3, dtype=torch.float32, device=bead2atom_relative_vectors.device)
-            reconstructed_atom_pos[:] = torch.nan
 
             b2a_idcs_row, b2a_idcs_col = torch.where(b2a_idcs>=0)
-            b2a_idcs_col += atom_pos_from
-            reconstructed_atom_pos[b2a_idcs_row, b2a_idcs[b2a_idcs_row, b2a_idcs_col]] = bead_pos[center_atoms][b2a_idcs_row]
+            reconstructed_atom_pos[b2a_idcs_row + bead_pos_from, b2a_idcs[b2a_idcs_row, b2a_idcs_col] + atom_pos_from] = bead_pos[center_atoms][b2a_idcs_row + bead_pos_from]
 
             batch_level_idcs_mask = level_idcs_mask[batch_center_atoms]
             batch_level_idcs_anchor_mask = level_idcs_anchor_mask[batch_center_atoms]
-            for level in range(batch_level_idcs_mask.size(1)):
-                if level == 0:
-                    continue
+            for level in range(1, batch_level_idcs_mask.size(1)):
                 level_idcs_mask_elem = batch_level_idcs_mask[:, level]
                 level_anchor_idcs_mask_elem = batch_level_idcs_anchor_mask[:, level]
                 mask_row, mask_col = torch.where(level_idcs_mask_elem)
                 if len(mask_row) > 0:
-                    # mask_col += atom_pos_from ?
-                    updated_pos = reconstructed_atom_pos[mask_row, level_anchor_idcs_mask_elem[mask_row, mask_col]]
+                    # Pick position of anchor points
+                    updated_pos = reconstructed_atom_pos[mask_row + bead_pos_from, level_anchor_idcs_mask_elem[mask_row, mask_col] + atom_pos_from]
+                    # Add predicted vector to positions
                     updated_pos = updated_pos + bead2atom_relative_vectors[center_atoms][mask_row, mask_col]
-                    reconstructed_atom_pos[mask_row, b2a_idcs[mask_row, mask_col]] = updated_pos
+                    # Store into the correct atom position
+                    reconstructed_atom_pos[mask_row + bead_pos_from, b2a_idcs[mask_row, mask_col] + atom_pos_from] = updated_pos
             
             # Re-center predicted atoms' center of mass to the actual bead position
-            # !!!!!!!!!!!!!!!!!!!!!!!!!! NOT WORKING WITH batch_size > 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             predicted_atoms_cm = scatter(
-                reconstructed_atom_pos[b2a_idcs_row, b2a_idcs[b2a_idcs_row, b2a_idcs_col]] * batch_weights[b2a_idcs_row, b2a_idcs_col][:, None],
+                reconstructed_atom_pos[b2a_idcs_row + bead_pos_from, b2a_idcs[b2a_idcs_row, b2a_idcs_col] + atom_pos_from] * batch_weights[b2a_idcs_row, b2a_idcs_col][:, None],
                 b2a_idcs_row,
                 dim=0,
+                dim_size=len(batch_center_atoms),
             )
-            atom_shifts = predicted_atoms_cm - bead_pos[center_atoms]
-            reconstructed_atom_pos[b2a_idcs_row, b2a_idcs[b2a_idcs_row, b2a_idcs_col]] -= atom_shifts[b2a_idcs_row]
+            atom_shifts = predicted_atoms_cm - bead_pos[batch_center_atoms]
+            reconstructed_atom_pos[b2a_idcs_row + bead_pos_from, b2a_idcs[b2a_idcs_row, b2a_idcs_col] + atom_pos_from] -= atom_shifts[b2a_idcs_row]
         
         data[self.out_field] = torch.nanmean(reconstructed_atom_pos, dim=0)
         return data
