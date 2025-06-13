f624c3810c46bc8bfc60445e774b96baae042a98
Daniangio <daniele.angioletti0@gmail.com>
2025-06-13 10:42:40 +0200

    updated tutorial configs to match the bead_is_next/bead_is_prev new input features

--- herobm/scripts/cg_analysis.py
+++ herobm/scripts/cg_analysis.py
@@ -11,26 +11,23 @@ from cgmap.scripts.map import aa2cg
 import tempfile
 
 
-def analyze_distances(input, mapping, output):
-    distances = defaultdict(list)
-
-    files = glob.glob(os.path.join(input, "*.pdb")) + glob.glob(os.path.join(input, "*.gro"))
-    for filename in files:
-        try:
-            print(f"Analysing CG distances for file {filename}...")
-            with tempfile.NamedTemporaryFile(suffix=".pdb", delete=True) as tmpfile:
-                tmp_filename = tmpfile.name
-                args_dict = {
-                "mapping": mapping,
-                "input": filename,
-                "output": tmp_filename,
-                "isatomistic": True,
-                }
-                cg_filename = aa2cg(args_dict)
-                
-                # The rest of the code using cg_filename goes here
+def compute_distances(distances, mapping, filename, inputtraj=None):
+    try:
+        print(f"Analysing CG distances for file {filename}...")
+        with tempfile.TemporaryDirectory() as tmpfolder:
+            args_dict = {
+            "mapping": mapping,
+            "input": filename,
+            "inputtraj": inputtraj,
+            "output": tmpfolder,
+            "isatomistic": True,
+            }
+            cg_filenames, cg_filename_trajs = aa2cg(args_dict)
+            
+            # The rest of the code using cg_filename goes here
+            for cg_filename, cg_filename_traj in zip(cg_filenames, cg_filename_trajs):
                 try:
-                    u = mda.Universe(cg_filename)
+                    u = mda.Universe(cg_filename, cg_filename_traj)
                     relevant_bead_names = sorted({atom.name for atom in u.atoms})
                     print("Universe loaded successfully.")
                 except FileNotFoundError:
@@ -42,38 +39,53 @@ def analyze_distances(input, mapping, output):
 
                 print("Calculating distances for connected beads...")
 
-                for segment in u.segments:
-                    for i in range(len(segment.residues)):
-                        res1 = segment.residues[i]
-                        # Intra-residue
-                        for j1 in range(len(relevant_bead_names) - 1):
-                            for j2 in range(j1 + 1, len(relevant_bead_names)):
-                                bead1_name = relevant_bead_names[j1]
-                                bead2_name = relevant_bead_names[j2]
-                                atom1 = res1.atoms.select_atoms(f"name {bead1_name}")
-                                atom2 = res1.atoms.select_atoms(f"name {bead2_name}")
-                                if atom1 and atom2:
-                                    atom1 = atom1[0]
-                                    atom2 = atom2[0]
-                                    dist = np.linalg.norm(atom2.position - atom1.position)
-                                    key = (res1.resname, bead1_name, res1.resname, bead2_name)
-                                    distances[key].append(dist)
-                        # Inter-residue
-                        if i + 1 < len(segment.residues):
-                            res2 = segment.residues[i+1]
-                            if res2.resid == res1.resid + 1:
-                                bb1 = res1.atoms.select_atoms("name BB")
-                                bb2 = res2.atoms.select_atoms("name BB")
-                                if bb1 and bb2:
-                                    bb_atom1 = bb1[0]
-                                    bb_atom2 = bb2[0]
-                                    dist = np.linalg.norm(bb_atom2.position - bb_atom1.position)
-                                    sorted_resnames = sorted([res1.resname, res2.resname])
-                                    key = (sorted_resnames[0], 'BB', sorted_resnames[1], 'BB')
-                                    distances[key].append(dist)
-                    
-                print("Completed.")
-        except: pass
+                for ts in u.trajectory:
+                    for segment in u.segments:
+                        for i in range(len(segment.residues)):
+                            res1 = segment.residues[i]
+                            # Intra-residue
+                            for j1 in range(len(relevant_bead_names) - 1):
+                                for j2 in range(j1 + 1, len(relevant_bead_names)):
+                                    bead1_name = relevant_bead_names[j1]
+                                    bead2_name = relevant_bead_names[j2]
+                                    atom1 = res1.atoms.select_atoms(f"name {bead1_name}")
+                                    atom2 = res1.atoms.select_atoms(f"name {bead2_name}")
+                                    if atom1 and atom2:
+                                        atom1 = atom1[0]
+                                        atom2 = atom2[0]
+                                        dist = np.linalg.norm(atom2.position - atom1.position)
+                                        key = (res1.resname, bead1_name, res1.resname, bead2_name)
+                                        distances[key].append(dist)
+                            # Inter-residue
+                            if i + 1 < len(segment.residues):
+                                res2 = segment.residues[i+1]
+                                if res2.resid == res1.resid + 1:
+                                    bb1 = res1.atoms.select_atoms("name BB")
+                                    bb2 = res2.atoms.select_atoms("name BB")
+                                    if bb1 and bb2:
+                                        bb_atom1 = bb1[0]
+                                        bb_atom2 = bb2[0]
+                                        dist = np.linalg.norm(bb_atom2.position - bb_atom1.position)
+                                        sorted_resnames = sorted([res1.resname, res2.resname])
+                                        key = (sorted_resnames[0], 'BB', sorted_resnames[1], 'BB')
+                                        distances[key].append(dist)
+        print("Completed.")
+        return distances
+    except Exception as e:
+        print(e)
+        pass
+
+def analyze_distances(input, inputtraj, mapping, output):
+    distances = defaultdict(list)
+
+    if os.path.isdir(input):
+        files = glob.glob(os.path.join(input, "*.pdb")) + glob.glob(os.path.join(input, "*.gro"))
+        for filename in files:
+            distances = compute_distances(distances, mapping, filename)
+    elif input.endswith((".pdb", ".gro")) and os.path.isfile(input):
+        distances = compute_distances(distances, mapping, input, inputtraj=inputtraj)
+    else:
+        raise ValueError("Input must be a folder or a .pdb/.gro file")
 
     print("Finished calculating distances.")
     print("Aggregating results and computing statistics...")
@@ -105,9 +117,10 @@ def main():
     parser.add_argument("-m", "--mapping", required=True, type=Path, help="Name of the CG mapping.\n"+
                         "It corresponds to the name of the chosen folder relative to the cgmap/data/ folder.\n"+
                         "Optionally, the user can specify its own mapping folder by providing an absolute path.",)
+    parser.add_argument("-t", "--inputtraj", help="Input trjectory file or folder, which contains all input traj files.")
     parser.add_argument("-o", "--output", default="out.csv", help="Output CSV file (default: out.csv)")
     args = parser.parse_args()
-    analyze_distances(args.input, args.mapping, args.output)
+    analyze_distances(args.input, args.inputtraj, args.mapping, args.output)
 
 if __name__ == "__main__":
     main()
